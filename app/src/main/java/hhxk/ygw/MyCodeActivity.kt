41b32ea423353d75376148d0860bd2ce347aeebc
Lyh <yvhao_liu@163.com>
2018-10-26 17:02:10 +0800

    .

--- app/src/main/java/hhxk/ygw/MyCodeActivity.kt
+++ app/src/main/java/hhxk/ygw/MyCodeActivity.kt
@@ -5,14 +5,21 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.RequiresApi
+import com.alibaba.fastjson.JSONObject
+import com.ohmerhe.kolley.request.Http
 import com.uuzuche.lib_zxing.activity.CodeUtils
 import hhxk.util.ActiivtyStack
+import hhxk.util.HeadUrl
+import hhxk.util.LoadingDialog
 import kotlinx.android.synthetic.main.activity_my_code.*
+import org.jetbrains.anko.toast
+import java.nio.charset.Charset
 
 /**
  * 我的二维码
  */
 class MyCodeActivity : AppCompatActivity() {
+    lateinit var dialog: LoadingDialog
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +28,39 @@ class MyCodeActivity : AppCompatActivity() {
         ActiivtyStack.getScreenManager().pushActivity(this)
         window.statusBarColor = resources.getColor(R.color.DimGray)
         initView()
+        initData()
     }
 
     fun initView() {
         back.setOnClickListener { finish() }
-        uImg.setImageURI("https://i01piccdn.sogoucdn.com/6c772fd3a9a2f3bf")
+        dialog=LoadingDialog(this)
         code.setImageBitmap(CodeUtils.createImage("15291879667", 400, 400, BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)))
+    }
+
+    fun initData(){
+        Http.get {
+            url = HeadUrl.url + "/personalCenter"
+            params {
+                "phone" - HeadUrl.loginName
+            }
+            onStart { dialog.show() }
+            onSuccess {
+                var str = it.toString(Charset.defaultCharset())
+                var j = JSONObject.parseObject(str)
+                when (j["code"]) {
+                    10000 -> {
+                        var data = j.getJSONObject("data")
+                        uImg.setImageURI(data["photo"].toString())
+                        name.text = data["realName"].toString()
+                    }
+                    else -> {
+                        toast(j["message"].toString())
+                    }
+                }
+            }
+            onFail { toast("请求失败！") }
+            onFinish { dialog.dismiss() }
+        }
     }
 
     override fun onDestroy() {
